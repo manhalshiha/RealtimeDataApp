22509e5e40c0aa07869a79d96e6bdff4334762ef
manhal shiha <manhal6@gmail.com>
2023-08-10 13:51:52 +0300

    Add project files.

---
+++ RealtimeDataApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealtimeDataApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        string _connectioString = "Server=DESKTOP-N02OMRJ\\SQLEXPRESS;Database=CompanyDatabase;Trusted_Connection=True;";
+
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectioString);
+        }
+    }
+}
---
+++ RealtimeDataApp/Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealtimeDataApp.Data;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace RealtimeDataApp.Services
+{
+    public class EmployeeService
+    {
+        AppDbContext dbContext = new AppDbContext();
+        public readonly SqlTableDependency<Employee> _dependency;
+        private readonly string _connectioString;
+        public EmployeeService()
+        {
+           _connectioString= "Server=DESKTOP-N02OMRJ\\SQLEXPRESS;Database=CompanyDatabase;Trusted_Connection=True;";
+            _dependency = new SqlTableDependency<Employee>(_connectioString, "employee");
+            _dependency.OnChanged += Changed;
+            _dependency.Start();
+        }
+
+        private void Changed(object sender, RecordChangedEventArgs<Employee> e)
+        {
+            int Value = 0;
+        }
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await dbContext.Employee.AsNoTracking().ToListAsync();
+        }
+
+    }
+}
